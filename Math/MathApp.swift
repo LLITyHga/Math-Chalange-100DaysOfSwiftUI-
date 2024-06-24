7c7cf4969ce2551e5095e0bac2adcde3295cc691
Андрій Пікусь <llityhga@gmail.com>
2024-06-24 15:38:59 +0300

    Initial Commit

---
+++ Math/MathApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathApp.swift
+//  Math
+//
+//  Created by Wolf on 24.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct MathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
